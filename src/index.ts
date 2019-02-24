c7fae06e4a29869608d43cf3a0ced8b0d81a2632
Simone Picciani <simone.picciani@buildo.io>
2019-02-24 14:39:00 +0100

    image sizes, err Maximum call stack size exceeded

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,84 @@
-import { TaskEither, taskify, tryCatch } from "fp-ts/lib/TaskEither";
+import { TaskEither, taskify, taskEither } from "fp-ts/lib/TaskEither";
+import { array } from "fp-ts/lib/Array";
+import { traverse } from "fp-ts/lib/Traversable";
 import { exec } from "child_process";
+import * as sizeOf from "image-size";
+import * as fs from "fs";
 
-const findDimensions = (dir: string): string =>
-  `find ${dir} -type f -exec identify -format "%f %w" \{\} \;`;
+const dir = `/Users/simonepicciani/Dropbox (Personal)/IFTTT/reddit/wallpapers`;
 
-export interface ExecResult {
-  stdout: Buffer | string;
-  stderr: Buffer | string;
-}
-const execTask = taskify(
-  (
-    command: string,
-    cb: (e: Error | null | undefined, r?: ExecResult) => void,
-  ) => {
-    exec(command, (err, stdout, stderr) => cb(err, { stdout, stderr }));
-  },
-);
+// export interface ExecResult {
+//   stdout: Buffer | string;
+//   stderr: Buffer | string;
+// }
+// const execTask = taskify(
+//   (
+//     command: string,
+//     cb: (e: Error | null | undefined, r?: ExecResult) => void,
+//   ) => {
+//     exec(command, (err, stdout, stderr) => cb(err, { stdout, stderr }));
+//   },
+// );
 
-function execTE(command: string): TaskEither<Error, string> {
-  return execTask(command).map(({ stdout }) => {
-    let res;
-    if (Buffer.isBuffer(stdout)) {
-      res = stdout.toString("utf8");
-    } else {
-      res = stdout;
-    }
-    return res;
-  });
-}
+// function execTE(command: string): TaskEither<Error, string> {
+//   return execTask(command).map(({ stdout }) => {
+//     let res;
+//     if (Buffer.isBuffer(stdout)) {
+//       res = stdout.toString("utf8");
+//     } else {
+//       res = stdout;
+//     }
+//     return res;
+//   });
+// }
 
-function splitAndFilter(stdout: string): string[] {
-  return stdout
-    .split("\n")
-    .filter(
-      x => x.length > 1 && x !== "1" && x !== "900.0" && !x.startsWith("~"),
-    );
-}
+const readDir: (
+  path: fs.PathLike,
+  options: { withFileTypes: true },
+) => TaskEither<NodeJS.ErrnoException, fs.Dirent[]> = taskify(fs.readdir);
 
-const dir = `~/Dropbox\ (Personal)/IFTTT/reddit/wallpapers`;
+const getDimensions = taskify(sizeOf);
+
+const filterOnlyFiles: (a: fs.Dirent[]) => fs.Dirent[] = files =>
+  files.filter(file => file.isFile());
+
+const traverseTEA = traverse(taskEither, array);
+
 function main(): void {
-  execTE(findDimensions(dir))
-    .map(x => {
-      console.log(x);
-      return x;
-    })
-    // .map(splitAndFilter)
-    // .chain(({ wallpaper }) =>
-    //   execTE(openWallpaperCommand(wallpaper)).map(() => wallpaper),
-    // )
+  readDir(dir, { withFileTypes: true })
+    .map(filterOnlyFiles)
+    .map(files => files.map(file => `${dir}/${file.name}`))
+    .chain(paths => traverseTEA(paths, singleFile => getDimensions(singleFile)))
+
     .fold(
       err => console.error("ERROR!!", JSON.stringify(err)),
-      res => console.log(`Successfuly opened ${res}`),
+      res => console.log(`res is: `, res),
     )
     .run();
 }
 
 main();
+
+// fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+//   if (err) {
+//     console.log("fail#readdir", JSON.stringify(err));
+//     return;
+//   }
+//   console.log(`found #${files.length} elements`);
+
+//   files
+//     .filter(file => file.isFile())
+//     .forEach(file => {
+//       sizeOf(`${dir}/${file.name}`, function(err, dimensions) {
+//         if (err) {
+//           console.log("fail#sizeOf", JSON.stringify(err));
+//           return;
+//         }
+//         if (dimensions.width < 1599) {
+//           console.log(
+//             `small image found -> [${file.name}] width: [${dimensions.width}]`,
+//           );
+//         }
+//       });
+//     });
+// });
